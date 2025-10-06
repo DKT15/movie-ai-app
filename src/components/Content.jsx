80b7c851eee72e00efeb6c78de68e2c3337d4273
Danté Kellman-Thompson <dante.kellman@icloud.com>
2025-10-06 18:30:50 +0100

    added to the code.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,28 +3,31 @@ import "../styles/Content";
 
 export default function Content() {
   const [loading, setLoading] = React.useState(false); // tracking the loading state
+  const [favouriteInputText, setFavouriteInputText] = React.useState(""); //state for the favourite input
+  const [moodInputText, setMoodInputText] = React.useState(""); //state for the favourite input
+  const [movieInputText, setMovieInputText] = React.useState(""); //state for the favourite input
 
   return (
     <>
       <h2>What is your favourite movie and why?</h2>
       <textarea
         className="favourite-movie"
-        // value=
-        // onChange=
+        value={favouriteInputText} // setting the value of state.
+        onChange={(e) => setFavouriteInputText(e.target.value)} // updating state to whatever the user types
         aria-label="Enter your favourite movie and why"
       ></textarea>
       <h2>Are you in the mood for something new or a classic?</h2>
       <textarea
         className="mood-movie"
-        // value=
-        // onChange=
+        value={moodInputText}
+        onChange={(e) => setMoodInputText(e.target.value)}
         aria-label="Enter if you are in the mood for something new or a classic"
       ></textarea>
       <h2>Do you want to have fun or do you want something serious</h2>
       <textarea
         className="movie"
-        // value=
-        // onChange=
+        value={movieInputText}
+        onChange={(e) => setMovieInputText(e.target.value)}
         aria-label="Enter if you want to have fun or do you want something serious"
       ></textarea>
       <button
